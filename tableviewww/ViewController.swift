7e1ca57b998066ce46e6538bd910278dd6c9cbca
Sai chandra <saichandra8121@gmail.com>
2022-03-08 13:03:56 +0530

    change println

--- tableviewww/ViewController.swift
+++ tableviewww/ViewController.swift
@@ -2,17 +2,31 @@
 //  ViewController.swift
 //  tableviewww
 //
-//  Created by Padakanti Sai Chandra on 08/03/22.
+//  Created by Padakanti Sai Chandra on 07/03/22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableviewww: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "idnum")
+        cell?.textLabel?.text = "idnum \(indexPath.row)"
+        return cell!
+    }
+    
 
 
 }
